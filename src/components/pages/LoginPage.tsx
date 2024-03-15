d3e771ec5544614d16e35dc27ff742174b091c1b
yuya024 <yuyaaanissy@gmail.com>
2024-03-15 16:05:13 +0900

    feat: ログイン画面を作成

--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -1,3 +1,28 @@
+import { PrimaryBtn } from "../atoms/PrimaryBtn";
+
 export const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div className="w-[500px] bg-white rounded-lg shadow-lg py-10">
+      <form className="flex flex-col justify-center items-center gap-10">
+        <h1 className="text-3xl text-lime-800 font-bold text-center">
+          ログイン
+        </h1>
+        <div className="w-[80%]">
+          <input
+            type="text"
+            placeholder="email"
+            className="w-full border-4 border-solid border-lime-800 rounded-md p-2"
+          />
+        </div>
+        <div className="w-[80%]">
+          <input
+            type="password"
+            placeholder="password"
+            className="w-full border-4 border-solid border-lime-800 rounded-md p-2"
+          />
+        </div>
+        <PrimaryBtn>ログイン</PrimaryBtn>
+      </form>
+    </div>
+  );
 };
