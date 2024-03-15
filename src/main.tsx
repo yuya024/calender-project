967b7698ed84353d9caa1b5b8c32b9d053465b79
yuya024 <yuyaaanissy@gmail.com>
2024-03-15 13:13:13 +0900

    feat: tailwindcssを導入

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/index.css";
 import "./styles/destyle.css";
+import "./styles/output.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode></React.StrictMode>
+  <React.StrictMode>
+    <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  </React.StrictMode>
 );
