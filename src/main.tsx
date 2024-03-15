9ae3199edb9b29791f988362221fe6f5e74da34f
yuya024 <yuyaaanissy@gmail.com>
2024-03-15 12:08:31 +0900

    init: Reactアプリの初期化

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/index.css";
+import "./styles/destyle.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode></React.StrictMode>
+);
