90b4e9045913215672fdd434e53b23a729213819
yuya024 <yuyaaanissy@gmail.com>
2024-03-16 14:38:33 +0900

    feat: ログインフォームをstateにて管理できるようにし、ログイン失敗時にエラーハンドリングを追加

---
+++ src/api/login.ts
@@ -0,0 +1,10 @@
+import { LoginInfoType, LoginReturnType } from "../types/login";
+
+export const login = (info: LoginInfoType): LoginReturnType => {
+  const { email, password } = info;
+  if (email === "test@example.com" && password === "password") {
+    return { id: 1, name: "sample太郎" };
+  } else {
+    throw new Error();
+  }
+};
